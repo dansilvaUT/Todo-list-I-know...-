e56dea706937436644ff6425411569a8d75fab2c
dansilvaUT <dan13silv@gmail.com>
2022-06-15 21:07:02 -0600

    set up some

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "static#index"
+
+  namespace :api do
+    #Todos
+    post :create_todo, to: "todos#create_todo"
+  end
 end
---
+++ db/migrate/20220616030109_create_todos.rb
@@ -0,0 +1,13 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :description
+      t.string :due_date
+      t.boolean :completed
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
