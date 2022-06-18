48d0b006311137236c6abc9aa365335abc3f9042
dansilvaUT <dan13silv@gmail.com>
2022-06-17 22:26:41 -0600

    todos set up.

--- app/controllers/user_module.rb
+++ app/controllers/user_module.rb
@@ -10,7 +10,7 @@ module UserModule
     def current_user
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
-
+    
     def authorized_user?
         @user == current_user
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_204300) do
+ActiveRecord::Schema.define(version: 2022_06_17_224801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2022_06_16_204300) do
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "due_date"
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -38,4 +49,5 @@ ActiveRecord::Schema.define(version: 2022_06_16_204300) do
   end
 
   add_foreign_key "tasks", "users"
+  add_foreign_key "todos", "users"
 end
